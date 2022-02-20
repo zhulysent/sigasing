0e48bbb7b56f6bf5c768539e9501fd1953fd4246
zhulysent <jazuli.uniska@gmail.com>
2022-02-20 13:35:13 +0700

    Pertemuan 1

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<?php include "partials/head.php"; ?>
+
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+ <?php include "partials/nav.php"; ?>
+ <?php include "partials/sidebar.php"; ?>
+  <!-- Content Wrapper. Contains page content -->
+ <div class="content-wrapper">
+   <?php include "routes.php"; ?>
+ </div>
+  <!-- /.content-wrapper -->
+ <?php include "partials/control.php"; ?>
+ <?php include "partials/footer.php"; ?>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!--?php include "partials/script.php"; ?-->
+</body>
+</html>
---
+++ partials/footer.php
@@ -0,0 +1,6 @@
+ <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <Strong>Version</strong> 0.1.0
+    </div>
+    <strong>Copyright &copy; <?= date('Y');?> <a href="javascript:void(0);">Juli Susanto</a>.</strong> All rights reserved.
+  </footer>
